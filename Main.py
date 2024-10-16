247ec675be7bfda15b3337c9403675c18ac09c27
AugustoBottazzi <augustobottazzi16@gmail.com>
2024-10-16 12:46:28 -0300

    Main.py

--- Main.py
+++ Main.py
@@ -13,14 +13,19 @@ print (f"Mostrar Maximo por tipo: {Max}")
 deposito, recaudacion = Funciones.deposito_mayor_recaudacion
 Funciones.contar_depositos_excedidos
 
-#Funciones.provincias_excedidas
-#if provincias_excedidas :
-    #print("\nProvincias que han almacenado mas de 50.000 unidades: ")
-#for i in range(len(provincias_excedidas [i])):
-    #print(provincias_excedidas [i])
-#else:
-    #print("\nNo hay provincias que hayan almacenado mas de 50.000 unidades. ")
+matriz_provincias = []
 
+    #Funciones.provincias_excedidas
+    #if provincias_excedidas :
+        #print("\nProvincias que han almacenado mas de 50.000 unidades: ")
+    #for i in range(len(provincias_excedidas [i])):
+        #print(provincias_excedidas [i])
+    #else:
+        #print("\nNo hay provincias que hayan almacenado mas de 50.000 unidades. ")
+    
+    print("Matriz Original: ")
+    for fila in existencias:
+        print(fila)
 
 
 
